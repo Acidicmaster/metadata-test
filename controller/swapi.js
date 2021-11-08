9f5b02721ea2e8d0ab85e03c0847dd06ee5e185c
Kenneth uche <kennethuche06@gmail.com>
2021-11-08 14:39:13 +0100

    swapi

--- controller/swapi.js
+++ controller/swapi.js
@@ -42,7 +42,7 @@ function dynamicSort(property) {
         sortOrder = -1;
         property = property.substr(1);
     }
-    return  (a,b)=> {
+    return  (a,b)=> { 
      
         var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
         return result * sortOrder;
